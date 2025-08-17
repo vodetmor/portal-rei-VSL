7a76a433949f93e02475dc27b72938153add807b
Vitor Machado Neves <vitormachado743@gmail.com>
2025-08-17 18:19:57 +0000

    Quero que a fonte do site seja SFPRODISPLAYHEAVYITALIC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
-      </head>
+      <head />
       <body className="font-body antialiased">
         {children}
         <Toaster />
