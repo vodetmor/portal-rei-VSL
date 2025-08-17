a3ff60306f762ba368a5e033c1826f6688acf88a
Vitor Machado Neves <vitormachado743@gmail.com>
2025-08-17 18:37:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
-import { Header } from '@/components/header';
 import { HeroSection } from '@/components/hero-section';
 import { SkillsSection } from '@/components/skills-section';
 import { PortfolioSection } from '@/components/portfolio-section';
-import { ClientsSection } from '@/components/clients-section';
 import { ContactSection } from '@/components/contact-section';
 import { Footer } from '@/components/footer';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh bg-background">
-      <Header />
       <main className="flex-1">
         <HeroSection />
         <SkillsSection />
         <PortfolioSection />
-        <ClientsSection />
         <ContactSection />
       </main>
       <Footer />
