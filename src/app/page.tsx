c381aca89e0fa3c29171f537b2790e06541a4b1f
Vitor Machado Neves <vitormachado743@gmail.com>
2025-10-26 04:02:19 +0000

    Extraia apenas o mundo e ajuste o posicionamento para que faça sentido c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,19 +98,16 @@ export default function Home() {
         </motion.div>
       </section>
 
-      <motion.section 
+      <motion.section
         initial={{ opacity: 0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeInOut" }}
-        className="w-full max-w-6xl py-24 px-4"
+        className="relative flex h-[550px] w-full flex-col items-center justify-center overflow-hidden"
       >
-        <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-40 pb-40 pt-8 md:pb-60 md:shadow-xl mx-auto">
-          <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-            Insights
-          </span>
-          <Globe className="top-28" />
-          <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+        <div className="relative flex h-full w-full items-center justify-center">
+            <Globe />
         </div>
+        <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
       </motion.section>
 
       <motion.section 
