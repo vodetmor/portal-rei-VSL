7baf21db175c8ce13af8d6eb7fa23b7675df26d5
Vitor Machado Neves <vitormachado743@gmail.com>
2025-10-26 05:15:58 +0000

    Olá! Para resolver o erro GenkitError: NOT_FOUND: Model 'gemini-1.5-flas

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,7 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({apiKey: process.env.GEMINI_API_KEY})
+  ],
 });
