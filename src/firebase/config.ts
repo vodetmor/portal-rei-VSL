866a3b84c6229aeea15b8324b35cf429ae6e5207
Vitor Machado Neves <vitormachado743@gmail.com>
2025-12-01 17:51:05 +0000

    Firebase Storage: No default bucket found. Did you set the 'storageBucke

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "hades-editor",
   "appId": "1:109170142345:web:23de0054b11c8584356a58",
+  "storageBucket": "hades-editor.appspot.com",
   "apiKey": "AIzaSyCs6R5UTyYJnWEyTLIqdoQ5DnyNdxkXsBw",
   "authDomain": "hades-editor.firebaseapp.com",
   "measurementId": "",
